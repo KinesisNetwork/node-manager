1acfc156e6394e62659108993c3f5330b8895c19
Arpad <arpad.toth@abx.com>
2018-07-09 15:27:45 +1000

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk'
-import { exec } from 'child_process'
+import { ChildProcess, exec } from 'child_process'
 import * as figlet from 'figlet'
 import * as flatten from 'flat'
 import * as inquirer from 'inquirer'
@@ -14,61 +14,77 @@ figlet('KINESIS MANAGER', (_, d) => {
   initialisedVorpal
     .command('start_node', 'Boot a node against a Kinesis network')
     .action(async () => {
-      exec('docker', (error) => {
-        if (error) {
-          initialisedVorpal.log('Docker must be installed to continue')
-          initialisedVorpal.ui.cancel()
+      checkIfDockerIsInstalled()
+
+      const networksAndRegions: any = await getNetworksAndRegions()
+      const networkChosen: string = (await chooseNetwork(Object.keys(networksAndRegions))).network
+
+      const flattenedNodes: any = flatten(
+        networksAndRegions[networkChosen],
+        {
+          delimiter: '-',
+          maxDepth: 2
         }
-      })
+      )
 
-      const networks = await rp({
-        uri: 'https://s3-ap-southeast-2.amazonaws.com/kinesis-config/keypairs/nodes.json',
-        method: 'GET',
-        json: true,
-      })
-      const keys: string[] = Object.keys(networks)
-
-      const networkChosen = (await inquirer
-        .prompt([
-          {
-            type: 'list',
-            name: 'network',
-            message: 'Which network do you choose?',
-            choices: keys
-          },
-        ])).network
-      const availableNodes = networks[networkChosen]
-      const flattenedNodes = flatten(availableNodes, { delimiter: '-', maxDepth: 2 })
-
-      const nodesSelected = await inquirer
-        .prompt([
-          {
-            type: 'checkbox',
-            message: 'Select nodes',
-            name: 'nodes',
-            choices() {
-              const result = []
-              for (const key in flattenedNodes) {
-                if (flattenedNodes[key]) {
-                  result.push(flattenedNodes[key])
-                }
-              }
-              return result
-            },
-            validate(answer) {
-              if (answer.length < 6) {
-                return 'You must select at least six nodes.'
-              }
-              return true
-            }
-          }
-        ])
+      const nodesSelected = await selectNodes(flattenedNodes)
 
       initialisedVorpal.log(chalk.blue(JSON.stringify(nodesSelected, null, 2)))
-
+      initialisedVorpal.log(chalk.yellow(JSON.stringify(flattenedNodes, null, 2)))
     })
 
   initialisedVorpal
     .delimiter('Kinesis $')
     .show()
 })
+
+function checkIfDockerIsInstalled(): ChildProcess {
+  return exec('docker', (error: Error) => {
+    if (error) {
+      initialisedVorpal.log('To continue, please install Docker!')
+      initialisedVorpal.ui.cancel()
+    }
+  })
+}
+
+function getNetworksAndRegions(): Promise<any> {
+  return rp({
+    uri: 'https://s3-ap-southeast-2.amazonaws.com/kinesis-config/keypairs/nodes.json',
+    method: 'GET',
+    json: true,
+  })
+}
+
+function chooseNetwork(keys: string[]): Promise<any> {
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'network',
+        message: 'Which network do you choose?',
+        choices: keys
+      },
+    ])
+}
+
+function hasAtLeastSixNodesSelected(selectedNodes: string[]): any {
+  if (selectedNodes.length < 6) {
+    return 'You must select at least six nodes.'
+  }
+  return true
+}
+
+function selectNodes(nodesList: any): Promise<any> {
+  return inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        message: 'Select nodes',
+        name: 'nodes',
+        choices: Object.keys(nodesList),
+        validate(answer) {
+          return hasAtLeastSixNodesSelected(answer)
+        }
+      }
+    ])
+}
