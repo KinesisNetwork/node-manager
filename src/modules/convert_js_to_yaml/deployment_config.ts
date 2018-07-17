2fcc3822369a124c528ac17064f0ef6032294830
Arpad <arpad.toth@abx.com>
2018-07-17 14:26:07 +1000

    Remove own node from history peers

--- src/modules/convert_js_to_yaml/deployment_config.ts
+++ src/modules/convert_js_to_yaml/deployment_config.ts
@@ -4,7 +4,6 @@ export default function getDeploymentConfig(deploymentConfigVariables: Deploymen
     ...deploymentConfigVariables.selectedNodesConfigs.publicKeys
   ]
   const historyPeers = [
-    deploymentConfigVariables.nodeNameByUser,
     ...deploymentConfigVariables.selectedNodesConfigs.names
   ]
 
