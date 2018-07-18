ae3d01c6133a9be90fa6f31de04b09f22d9faa07
Arpad <arpad.toth@abx.com>
2018-07-19 09:57:04 +1000

    Add test case for advanced node command

---
+++ src/test/commands/advanced_node.spec.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+// import * as fs from 'fs'
+import * as mockFs from 'mock-fs'
+
+import { getLocalConfigFile } from '../../commands/advanced_node'
+
+describe('check if local config exists', () => {
+  it('#getLocalConfigFile returns the config object if exists', () => {
+    const networkConfig = JSON.stringify({ version: 3 })
+    const setUpRootDirectory = () => {
+      return mockFs({
+        'network_config.json': networkConfig
+      })
+    }
+    setUpRootDirectory()
+
+    const localConfig = getLocalConfigFile()
+    const expectedConfig = { version: 3 }
+
+    expect(localConfig).to.deep.equal(expectedConfig)
+
+    mockFs.restore()
+  })
+
+  it('#getLocalConfigFile throws an error if the file does not exist', () => {
+    const setUpRootDirectory = () => {
+      return mockFs()
+    }
+    setUpRootDirectory()
+
+    expect(getLocalConfigFile).to.throw()
+
+    mockFs.restore()
+  })
+})
